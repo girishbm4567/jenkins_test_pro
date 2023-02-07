1dc98a1432802423a9983e6135d248fdd7b3e4d7
girishbm4567 <113753419+girishbm4567@users.noreply.github.com>
2023-02-07 21:59:01 +0530

    new commit

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,6 +21,6 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Girish this is my first build and deploy");
+	  System.out.println("Hello from Girish this is my first build and deploy in local");
   }
 }
